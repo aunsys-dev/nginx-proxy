c320b222e94cfa3f18ed1901d6d0b4fced18709b
root <aunsys-dev>
2022-05-08 23:42:25 +0700

    proxy non port

--- proxy-nonport.sh
+++ proxy-nonport.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo Input domain tujuan:
+echo Input IP server tujuan:
 read proxy_pass
 
 echo Input port proxy:
@@ -17,7 +17,7 @@ rm /etc/nginx/sites-available/proxy.conf
 rm /etc/nginx/sites-enabled/proxy.conf
 
 wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/aunsys-dev/nginx-proxy/master/nginx.conf"
-wget -O /etc/nginx/sites-available/proxy.conf "https://raw.githubusercontent.com/aunsys-dev/nginx-proxy/master/proxy-https.conf"
+wget -O /etc/nginx/sites-available/proxy.conf "https://raw.githubusercontent.com/aunsys-dev/nginx-proxy/master/proxy-nonport.conf"
 ln -s /etc/nginx/sites-available/proxy.conf /etc/nginx/sites-enabled/
 service nginx restart
 
@@ -25,7 +25,6 @@ service nginx restart
 IP=`curl -s ifconfig.me`;
 
 sed -i "s/x.x.x.x/$IP/g" /etc/nginx/sites-available/proxy.conf
-sed -i "s/z.z.z.z/$port/g" /etc/nginx/sites-available/proxy.conf
 sed -i "s/y.y.y.y/$proxy_pass/g" /etc/nginx/sites-available/proxy.conf
 
 service nginx restart
